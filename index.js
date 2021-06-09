8bfed00ba959017fe9d65381bfbd5edaa5d01c8b
picsacoder <66335345+picsacoder@users.noreply.github.com>
2021-06-09 12:44:32 -0500

    Commit innecesario

--- index.js
+++ index.js
@@ -1,63 +1,11 @@
-function anterior(index, inicio) {
-    var nodo_actual = inicio;
-    var count = 0;
-
-    while (count < index-1) {
-        count++;
-        nodo_actual = nodo_actual.next;
-    };
-
-    
-
-    return nodo_actual.elem;
-};
-
-function getUltimo(inicio) {
-    if (!inicio) {
-        return null
-    };
-
-    let nodo_actual = inicio;
-
-    while (nodo_actual) {
-        if (!nodo_actual.next) {
-            return nodo_actual;
-        };
-
-        nodo_actual = nodo_actual.next;
-    }
-
-
-}
-
-
-function GetIndex(elem, inicio) { 
-    var nodo_actual = inicio;
-    var index = -1
-
-    while (nodo_actual) {
-        index++
-
-        if (nodo_actual.elem === elem) { 
-            console.log(index)
-            return index;
-        }
-
-        nodo_actual = nodo_actual.next;
-    };
-
-    return -1
-}
-
 function linked_list() { 
-    var longitud = 0;
     var inicio = null;
-
-
+    var cola = null
 
     var Nodo = function(elem) { 
         this.elem = elem;
         this.next = null;
+        this.prev = null
 
     };
 
@@ -65,24 +13,20 @@ function linked_list() {
         var nodo = new Nodo(elem) //Crea un nodo
 
 
-        if(inicio === null) { 
+        if(cola === null) { 
             inicio = nodo;
+            cola = nodo;
 
             
         } //Checa si el inicio de la lista esta vacio, en caso de que si el nodo pasa a ser el primer elemento
 
         else { 
-            var nodo_actual = inicio;
-                       
-            while (nodo_actual.next) {
-                nodo_actual = nodo_actual.next;
-            };
-
-            nodo_actual.next = nodo;
+            cola.next = nodo;
+            nodo.prev = cola;
+            cola = nodo;
             
         }
 
-        longitud++ //Se le añade +1 a la longitud de la lista
     };
 
     this.inicio = function() { 
@@ -91,46 +35,75 @@ function linked_list() {
     };
 
 
-    this.sort = function() {
-        
-        let last = getUltimo(inicio)
-        var nodo_actual = inicio;
+    this.sort = function() { 
+        let last = cola;
 
-        while (nodo_actual != last) { 
-            if (nodo_actual.elem) { }
+        while (last) { 
+            let nodo_actual = inicio;
 
+            while (nodo_actual != last) {
+                let sig = nodo_actual.next;
+                if (nodo_actual.elem > sig.elem) {
+                    [nodo_actual.elem, sig.elem] = [sig.elem, nodo_actual.elem]
+                }
 
-            if (last) {}
-            nodo_actual = nodo_actual.next;
+                nodo_actual = sig
+            };
+
+            last = last.prev
         }
-
-        
     };
+
 }
 
 lista = new linked_list();
 
 
 
-lista.anadir(3)
-lista.anadir(1)
-lista.anadir(5)
-lista.anadir(6)
+//lista.anadir(3)
+//lista.anadir(1)
+//lista.anadir(5)
+//lista.anadir(6)
 
 
-lista.inicio()
+//lista.inicio()
 
 
-console.log("=====================================================================================================================================================")
+//console.log("=====================================================================================================================================================")
 
-lista.sort()
+//lista.sort()
 
-lista.inicio()
+//lista.inicio()
 
 
 function anadir() {
     var value_to_add = document.getElementById("anadir");
 
+
+
+    //chek if is a number 
+
+    var is_number = parseInt(value_to_add.value);
+
+    if (!is_number) { 
+        alert("Introduzca un valor que sea un número")
+    }
+
+    else { 
+        var visual_node = document.createElement("p");
+        visual_node.innerHTML = value_to_add.value;
+
+        document.body.appendChild(visual_node)
+
+        lista.anadir(is_number)
+
+        lista.inicio()
+    }
+
     
 
+}
+
+function ordenar() { 
+    
 }
